35aca3bdf11e98298ee15223841499962cc77861
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2023-10-13 09:47:01 +0300

    asasd

--- frontend/src/context/Navbar/Navbar.jsx
+++ frontend/src/context/Navbar/Navbar.jsx
@@ -3,15 +3,10 @@ import phone from './../../pages/assets/IMG/Vector.png';
 import email from './../../pages/assets/IMG/Component.png';
 import logo from './../../pages/assets/IMG/Ranch.png';
 import { AiOutlineSearch } from 'react-icons/ai';
-import Home from '../../pages/Home/Home';
+import imgUZ from '../../pages/assets/IMG/Flags.png';
+
 
 export default function Navbar() {
-
-    const options = [
-        'one', 'two', 'three'
-    ]
-    const defaultOption = options[0];
-
     return (
         <div className='Navbar'>
             <div className="NavUp">
@@ -36,10 +31,28 @@ export default function Navbar() {
                     <button>
                         Kirish
                     </button>
+                    <select>
+                        <option value="option1">UZ  </option>
+                        <option value="option2" data-img-src={imgUZ}>Option 2</option>
+                        <option value="option3" data-img-src="/path/to/image3.jpg">Option 3</option>
+                    </select>
                 </div>
             </div>
             <div className="NavDown">
-
+                {/* <button className="DefaultLeng">
+                    <h5>UZ</h5> <img src={imgUZ} alt="" />
+                </button>
+                <div className='lengs'>
+                    <button className="DefaultLeng">
+                        <h5>UZ</h5> <img src={imgUZ} alt="" />
+                    </button>
+                    <button className="DefaultLeng">
+                        <h5>RU</h5> <img src={imgUZ} alt="" />
+                    </button>
+                    <button className="DefaultLeng">
+                        <h5>EN</h5> <img src={imgUZ} alt="" />
+                    </button>
+                </div> */}
             </div>
         </div>
     )
