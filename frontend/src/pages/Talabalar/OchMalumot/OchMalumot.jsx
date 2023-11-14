8ba2895be93ae2399ecbe2fc8fb5e52249f236d6
K A I N <73824592+Dark-dka@users.noreply.github.com>
2023-11-14 10:18:53 +0300

    ghj

--- frontend/src/pages/Talabalar/OchMalumot/OchMalumot.jsx
+++ frontend/src/pages/Talabalar/OchMalumot/OchMalumot.jsx
@@ -1,3 +1,9 @@
+import './OchMalumot.css';
+import { Link } from "react-router-dom";
+import { RiBankLine } from 'react-icons/ri';
+import { AiOutlineRight } from 'react-icons/ai';
+import axios from 'axios';
+
 export default function OchMalumot() {
     return (
         <div className="OchMalumot">
@@ -7,10 +13,79 @@ export default function OchMalumot() {
                     <AiOutlineRight className='Iconright' />
                     <Link className='NewsTitleLineText' to={'/'}>University</Link>
                     <AiOutlineRight className='Iconright' />
-                    <p>Imkoniyatlar</p>
+                    <p>Ochiq Ma’lumotlar</p>
                 </div>
                 <div className="OchMalumotContent">
-                    
+                    <div className="OMCTitle">
+                        <p>
+                            Soha hodimlarining huquqiy ongi va madaniyatini yuksaltirish bo’yicha seminarlar
+                        </p>
+                    </div>
+                    <div className="OMCBoxObsh">
+                        <div className="OMCBox">
+                            <div className="imgBoxOMC">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="TextBoxOMC">
+                                <p>Dekabir 6, 2022</p>
+                                <p> O‘zbekiston Respublikasi Konstitutsiyasining mazmun-mohiyatini tushuntirish bo‘yicha seminar ...</p>
+                            </div>
+                        </div>
+                        <div className="OMCBox">
+                            <div className="imgBoxOMC">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="TextBoxOMC">
+                                <p>Dekabir 6, 2022</p>
+                                <p> O‘zbekiston Respublikasi Konstitutsiyasining mazmun-mohiyatini tushuntirish bo‘yicha seminar ...</p>
+                            </div>
+                        </div>
+                        <div className="OMCBox">
+                            <div className="imgBoxOMC">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="TextBoxOMC">
+                                <p>Dekabir 6, 2022</p>
+                                <p> O‘zbekiston Respublikasi Konstitutsiyasining mazmun-mohiyatini tushuntirish bo‘yicha seminar ...</p>
+                            </div>
+                        </div>
+                        <div className="OMCBox">
+                            <div className="imgBoxOMC">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="TextBoxOMC">
+                                <p>Dekabir 6, 2022</p>
+                                <p> O‘zbekiston Respublikasi Konstitutsiyasining mazmun-mohiyatini tushuntirish bo‘yicha seminar ...</p>
+                            </div>
+                        </div>
+                        <div className="OMCBox">
+                            <div className="imgBoxOMC">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="TextBoxOMC">
+                                <p>Dekabir 6, 2022</p>
+                                <p> O‘zbekiston Respublikasi Konstitutsiyasining mazmun-mohiyatini tushuntirish bo‘yicha seminar ...</p>
+                            </div>
+                        </div>
+                        <div className="OMCBox">
+                            <div className="imgBoxOMC">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="TextBoxOMC">
+                                <p>Dekabir 6, 2022</p>
+                                <p> O‘zbekiston Respublikasi Konstitutsiyasining mazmun-mohiyatini tushuntirish bo‘yicha seminar ...</p>
+                            </div>
+                        </div>
+                        <div className="OMCBox">
+                            <div className="imgBoxOMC">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="TextBoxOMC">
+                                <p>Dekabir 6, 2022</p>
+                                <p> O‘zbekiston Respublikasi Konstitutsiyasining mazmun-mohiyatini tushuntirish bo‘yicha seminar ...</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
