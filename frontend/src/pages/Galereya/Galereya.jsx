c96dd96820a68d18bd82bfb0fdb811156768aed2
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2023-11-14 12:18:54 +0300

    tgrgtr

--- frontend/src/pages/Galereya/Galereya.jsx
+++ frontend/src/pages/Galereya/Galereya.jsx
@@ -1,5 +1,5 @@
 import './Galereya.css';
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { RiBankLine } from 'react-icons/ri';
 import { AiOutlineRight } from 'react-icons/ai';
 import axios from 'axios';
@@ -49,39 +49,55 @@ export default function Galereya() {
         setSelectedCategory(categoryId);
     }
 
+    const {id} = useParams()
+    const [gallaryData, setGallaryData] = useState([])
+    const getGallary = async () => {
+        const response = await axios.get(`https://utu-ranch.uz/api/content/${id}/`)
+        setGallaryData(response.data)
+        console.log(response.data);
+    }
+
+    useEffect(() => {
+        getGallary()
+    }, [id])
+
     return (
         <div className="Galereya">
-            <div className="GalereyaContainer">
-                <div className="NewsTitleLine">
-                    <Link className='NewsTitleLineIcon' to={'/'}><RiBankLine /></Link>
-                    <AiOutlineRight className='Iconright' />
-                    <Link className='NewsTitleLineText' to={'/'}>University</Link>
-                    <AiOutlineRight className='Iconright' />
-                    <p>Imkoniyatlar</p>
-                </div>
-                <div className="GalereyaContant">
-                    <aside>
-                        <p className='TitleAside'>Discover</p>
-                        <div className="AsideLink">
-                            {catData.map(item => (
-                                <Link key={item.id} onClick={() => handleCategoryClick(item.id)}>
-                                    <img src={zvezda} alt="" />
-                                    {item.name}
-                                </Link>
-                            ))}
+            {
+                gallaryData.map(data => (
+                    <div className="GalereyaContainer" key={data.id}>
+                        <div className="NewsTitleLine">
+                            <Link className='NewsTitleLineIcon' to={'/'}><RiBankLine /></Link>
+                            <AiOutlineRight className='Iconright' />
+                            <Link className='NewsTitleLineText' to={'/'}>University</Link>
+                            <AiOutlineRight className='Iconright' />
+                            <p>Imkoniyatlar</p>
                         </div>
-                    </aside>
-                    <div className="GalereyaCont">
-                        <div className="GalleryContImgBox">
-                            {images.map(item => (
-                                <div key={item.id} className="imgBox">
-                                    <img src={item.image} alt="" />
+                        <div className="GalereyaContant">
+                            <aside>
+                                <p className='TitleAside'>Discover</p>
+                                <div className="AsideLink">
+                                    {catData.map(item => (
+                                        <Link key={item.id} onClick={() => handleCategoryClick(item.id)}>
+                                            <img src={zvezda} alt="" />
+                                            {item.name}
+                                        </Link>
+                                    ))}
                                 </div>
-                            ))}
+                            </aside>
+                            <div className="GalereyaCont">
+                                <div className="GalleryContImgBox">
+                                    {images.map(item => (
+                                        <div key={item.id} className="imgBox">
+                                            <img src={item.image} alt="" />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                ))
+            }
         </div>
     );
 }
