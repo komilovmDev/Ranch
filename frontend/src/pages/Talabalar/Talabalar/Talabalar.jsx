c7f9a589399c708f119549f87d2623de366c30b4
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2023-11-15 08:30:49 +0300

    sdfsdf

--- frontend/src/pages/Talabalar/Talabalar/Talabalar.jsx
+++ frontend/src/pages/Talabalar/Talabalar/Talabalar.jsx
@@ -2,46 +2,39 @@ import "./Talabalar.css"
 import { Link, useParams } from "react-router-dom";
 import { RiBankLine } from 'react-icons/ri';
 import { AiOutlineRight } from 'react-icons/ai';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import fileSaver from 'file-saver';
+import axios from 'axios'
+import { useEffect, useState } from "react";
 
-//API key
-const { id } = useParams()
-const [allInfo, setAllInfo] = useState([])
-const getAlltalab = async () => {
-    const response = await axios.get(`https://utu-ranch.uz/api/content/${id}/`)
-    setAllInfo(response.data)
-    console.log(response.data);
-}
 
-useEffect(() => {
-    getAlltalab()
-}, [id])
-
-const [data, setData] = useState([])
-const getTalabalar = async () => {
-    const response = await axios.get(`https://utu-ranch.uz/api/talabalar/`)
-    console.log(response.data);
-    setData(response.data)
-}
-
-useEffect(() => {
-    getTalabalar()
-}, [id])
-
-const saveFile = (file) => {
-    fileSaver.saveAs(
-        process.env.REACT_APP_CLIENT_URL + `https://utu-ranch.uz/api/talabalar/${file}`,
-    );
-}
 
 export default function Talabalarga() {
+
+    const [data, setData] = useState([])
+    const getData = async () => {
+        const response = await axios.get(`https://utu-ranch.uz/api/talabalar/`)
+        setData(response.data)
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const { id } = useParams()
+    const [alldata, setallData] = useState([])
+    const GetallData = async () => {
+        const response = await axios.get(`https://utu-ranch.uz/api/content/${id}/`)
+        setallData(response.data)
+    }
+
+    useEffect(() => {
+        GetallData()
+    }, [id])
+
     return (
         <div className="Talabalarga">
             {
-                allInfo.map(info => (
-                    <div key={info.id} className="TalabalargaContainer">
+                alldata.map(infos => (
+                    <div key={infos.id} className="TalabalargaContainer">
                         <div className="NewsTitleLine">
                             <Link className='NewsTitleLineIcon' to={'/'}><RiBankLine /></Link>
                             <AiOutlineRight className='Iconright' />
@@ -51,12 +44,12 @@ export default function Talabalarga() {
                         </div>
                         <div className="TalabalarContent">
                             {
-                                allInfo.map(item => (
+                                data.map(item => (
                                     <div className="TalabalarFile">
                                         <div className="TalabalarPng">
-                                            
+                                            {item.title}
                                         </div>
-                                        <button onClick={() => saveFile(item.file)}>
+                                        <button >
                                             Download File
                                         </button>
                                     </div>
