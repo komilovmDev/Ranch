f982c7cfcb2ba2a382896c9fa85aa30dfb7103ed
K A I N <73824592+Dark-dka@users.noreply.github.com>
2023-11-27 08:26:39 +0300

    vivfvfiifpivpivfpisvpi

--- frontend/src/pages/Abiturient/Zayafka/Qabultugadi/Qabultugadi.jsx
+++ frontend/src/pages/Abiturient/Zayafka/Qabultugadi/Qabultugadi.jsx
@@ -6,6 +6,7 @@ export default function Qabultugadi() {
         <div className="Qabultugadi">
             <div className="QabultugadiTitle">
                 <h1>QABUL TUGADI'😢</h1>
+                <p>Malumot uchun <a href="tel">62 227 77 72</a> raqamiga murojaat qiling</p>
             </div>
         </div>
     )
